4888049da456a836c07de3c1bbef1e75dd2b063d
Angel Manuel Hinojosa Sánchez <hangel8000@gmail.com>
2026-03-24 23:11:17 -0600

    A1
    Mugre cosa no le entiendo nada

---
+++ CasuleCorp_A1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ya me perdi no se que estoy aciendo ayudenme ");
+        Console.ReadLine();
+    }
+}
